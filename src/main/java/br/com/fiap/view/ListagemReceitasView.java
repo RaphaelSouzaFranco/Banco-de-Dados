171f57d81558c11dfa8a624c8fb40343a677d467
RaphaelSouzaFranco <raphchio@gmail.com>
2025-04-21 17:45:52 -0300

    Mudanças na Classe Receitas, ReceitasDAO, Criação do CadastroReceitasView e ListagemReceitasView

---
+++ src/main/java/br/com/fiap/view/ListagemReceitasView.java
@@ -0,0 +1,13 @@
+package br.com.fiap.view;
+import br.com.fiap.dao.ReceitasDAO;
+import br.com.fiap.model.Receitas;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class ListagemReceitasView {
+    public static void main(String[] args) throws SQLException {
+        ReceitasDAO dao = new ReceitasDAO();
+
+    }
+}
