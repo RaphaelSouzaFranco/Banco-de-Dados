b0174828b9ccfad6b9c1f471b3614de225d71296
RaphaelSouzaFranco <raphchio@gmail.com>
2025-04-21 11:01:36 -0300

    Refatorar o Receitas

--- src/main/java/br/com/fiap/model/Receitas.java
+++ src/main/java/br/com/fiap/model/Receitas.java
@@ -1,14 +1,41 @@
 package br.com.fiap.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Receitas {
-    private double valor;
+    private int idReceitas ;
+    private BigDecimal valor;
     private String data;
     private String origem;
+    private Date vencimento;
+    private String categoriaReceitas;
+    private String descricao;
+    private String status;
+    private String formaPagamento;
 
-    public Receitas(double valor, String data, String origem) {
+
+    public Receitas(BigDecimal valor, String data, String origem, int idReceitas, Date vencimento, String categoriaReceitas, String descricao, String status, String formaPagamento  ) {
         this.valor = valor;
         this.data = data;
         this.origem = origem;
+        this.idReceitas = idReceitas;
+        this.vencimento = vencimento;
+        this.categoriaReceitas = categoriaReceitas;
+        this.descricao= descricao;
+        this.status= status;
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Receitas(double valor, String data, String origem) {
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
     }
 
     public String getData() {
@@ -19,17 +46,75 @@ public class Receitas {
         this.data = data;
     }
 
-    public double getValor() {
-        return valor;
-    }
-
     public String getOrigem() {
         return origem;
     }
 
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public Date getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(Date vencimento) {
+        this.vencimento = vencimento;
+    }
+
+    public String getCategoriaReceitas() {
+        return categoriaReceitas;
+    }
+
+    public void setCategoriaReceitas(String categoriaReceitas) {
+        this.categoriaReceitas = categoriaReceitas;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public int getIdReceitas() {
+        return idReceitas;
+    }
+
+    public void setIdReceitas(int idReceitas) {
+        this.idReceitas = idReceitas;
+    }
+
+    public Receitas() {
+    }
+
     public void exibirDados() {
+        System.out.println("id: "+ idReceitas);
         System.out.println("Valor: R$" + valor);
         System.out.println("Data: " + data);
         System.out.println("Origem: " + origem);
+        System.out.println("Vencimento: " + vencimento);
+        System.out.println("Categoria: " + categoriaReceitas);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Status: " + status);
+        System.out.println("Forma de Pagamento: "+ formaPagamento);
+
     }
 }
